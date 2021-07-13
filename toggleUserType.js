ee7194773a735e25c91da03f7a4089d9ae24ac16
rileytoddherman <rileyherman@tuta.io>
2021-07-13 13:13:41 -0600

    pill toggle

--- toggleUserType.js
+++ toggleUserType.js
@@ -1,17 +1,17 @@
 function showRenterContent() {
     document.getElementById("content").innerHTML = document.getElementById("renter").innerHTML;
-    document.getElementById("renter-pill").classList.remove("text-gray-600", "bg-white");
-    document.getElementById("renter-pill").classList.add("text-white", "tk-bg-teal");
-    document.getElementById("landlord-pill").classList.remove("text-white", "tk-bg-amber");
-    document.getElementById("landlord-pill").classList.add("text-gray-600", "bg-white");
+    document.getElementById("renter-pill").classList.remove("text-gray-500", "bg-white");
+    document.getElementById("renter-pill").classList.add("text-white", "tk-bg-teal", "shadow-lg");
+    document.getElementById("landlord-pill").classList.remove("text-white", "tk-bg-amber", "shadow-lg");
+    document.getElementById("landlord-pill").classList.add("text-gray-500", "bg-gray-100");
 }
 
 function showLandlordContent() {
     document.getElementById("content").innerHTML = document.getElementById("landlord").innerHTML;
-    document.getElementById("renter-pill").classList.remove("text-white", "tk-bg-teal");
-    document.getElementById("renter-pill").classList.add("text-gray-600", "bg-white");
-    document.getElementById("landlord-pill").classList.remove("text-gray-600", "bg-white");
-    document.getElementById("landlord-pill").classList.add("text-white", "tk-bg-amber");
+    document.getElementById("renter-pill").classList.remove("text-white", "tk-bg-teal", "shadow-lg");
+    document.getElementById("renter-pill").classList.add("text-gray-500", "bg-gray-100");
+    document.getElementById("landlord-pill").classList.remove("text-gray-500", "bg-gray-100");
+    document.getElementById("landlord-pill").classList.add("text-white", "tk-bg-amber", "shadow-lg");
 }
 
 window.onload = (event) => {

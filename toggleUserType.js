42fade99b27d47e232612fd91350736c3fe6902b
rileytoddherman <rileyherman@tuta.io>
2021-07-17 09:46:18 -0600

    hack the pains away

--- toggleUserType.js
+++ toggleUserType.js
@@ -4,7 +4,7 @@ function showRenterContent() {
     document.getElementById("renter-pill").classList.add("text-white", "tk-bg-teal", "shadow-lg");
     document.getElementById("landlord-pill").classList.remove("text-white", "tk-bg-amber", "shadow-lg");
     document.getElementById("landlord-pill").classList.add("text-gray-500", "bg-gray-100");
-    document.getElementById("download-label").classList.remove("tk-text-amber", " tk-text-blue");
+    document.getElementById("download-label").classList.remove("tk-text-amber");
     document.getElementById("download-label").classList.add("tk-text-teal");
 }
 
@@ -14,10 +14,6 @@ function showLandlordContent() {
     document.getElementById("renter-pill").classList.add("text-gray-500", "bg-gray-100");
     document.getElementById("landlord-pill").classList.remove("text-gray-500", "bg-gray-100");
     document.getElementById("landlord-pill").classList.add("text-white", "tk-bg-amber", "shadow-lg");
-    document.getElementById("download-label").classList.remove("tk-text-teal", " tk-text-blue");
+    document.getElementById("download-label").classList.remove("tk-text-teal");
     document.getElementById("download-label").classList.add("tk-text-amber");
 }
-
-window.onload = (event) => {
-    showRenterContent();
-};
